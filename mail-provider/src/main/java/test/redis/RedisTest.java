2a9e0a02ea1de081a276f02ab1370583147ae7a7
zhangmin <1115307804@qq.com>
2018-01-16 17:06:07 +0800

    更改简易的注册中心，初始化bean

--- mail-provider/src/main/java/test/redis/RedisTest.java
+++ mail-provider/src/main/java/test/redis/RedisTest.java
@@ -8,21 +8,29 @@ import redis.clients.jedis.Jedis;
  */
 public class RedisTest {
     public static void main(String[] args) {
-        //连接本地的reis服务
+//        String a = "abcdefg";
+//        System.out.println(a.startsWith("abc"));
+////        //连接本地的reis服务
+////        Jedis jedis = new Jedis("localhost");
+////        System.out.println("连接本地的Redis服务成功");
+////        //查看服务是不是正常
+////        System.out.println("服务 正在运行:"+jedis.ping());
+        //连接本地的redis 服务
         Jedis jedis = new Jedis("localhost");
-        System.out.println("连接本地的Redis服务成功");
-        //查看服务是不是正常
+        System.out.println("连接本地连接的redis 服务成功");
+        //查看服务是不是运行
         System.out.println("服务 正在运行:"+jedis.ping());
-
     }
     @Test
     public void TestRedisString() {
-        //连接本地服务
+        //连接本地的redis 服务
         Jedis jedis = new Jedis("localhost");
-        System.out.println("connecte to server");
+        System.out.println("connection to server successfully");
         //设置redis 字符串数据
-        //获取存储的数据并输出
-        String nmae
+        // jedis.set("runnoobkery","redis tutorial")
+        //
+        String name = jedis.get("theName");
+        System.out.println("stored theName in redis::"+name);
     }
 
 }
